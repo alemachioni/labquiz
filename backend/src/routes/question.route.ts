2bb4c0d0c1533a7d091071d63228898683ff3e08
Otavio7opes <otaviosdlopes@gmail.com>
2026-04-29 11:38:39 -0300

    Update question.route.ts

--- backend/src/routes/question.route.ts
+++ backend/src/routes/question.route.ts
@@ -2,13 +2,13 @@ typescript
 import { Router } from 'express'
 import { listarQuestoes, criarQuestao, atualizarQuestao, deletarQuestao } from '../controllers/question.controller'
 import { autenticar } from '../middlewares/auth.middleware'
-import { apenasProfesor } from '../middlewares/role.middleware'
+import { apenasProfessor } from '../middlewares/role.middleware'
 
 const router = Router()
 
 router.get('/', autenticar, listarQuestoes)
-router.post('/', autenticar, apenasProfesor, criarQuestao)
-router.put('/:id', autenticar, apenasProfesor, atualizarQuestao)
-router.delete('/:id', autenticar, apenasProfesor, deletarQuestao)
+router.post('/', autenticar, apenasProfessor, criarQuestao)
+router.put('/:id', autenticar, apenasProfessor, atualizarQuestao)
+router.delete('/:id', autenticar, apenasProfessor, deletarQuestao)
 
 export default router
